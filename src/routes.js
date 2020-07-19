7891e53effc7159d3437d497d553ea2d514e2198
Leonan Mattos <leocwb@MacBook-Air-de-Leonan.local>
2020-07-19 16:52:17 -0300

    Users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
+import User from './app/models/User';
+
 const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Leonan Mattos',
+    email: 'leonanmattos@gmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
