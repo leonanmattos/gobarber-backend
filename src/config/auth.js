514dc2b963838f978804e7b840798b143e685cc7
Leonan Mattos <leocwb@MacBook-Air-de-Leonan.local>
2020-07-22 00:08:41 -0300

    User and Session Controller complete

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9b8528ca4fba6f5d685a35bb1435e1c2',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Leonan Mattos',
-    email: 'leonanmattos@gmail.com',
-    password_hash: '123456789',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
