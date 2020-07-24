7e452ef7980279a95a8b0466b6d98a3683f82019
Leonan Mattos <leocwb@MacBook-Air-de-Leonan.local>
2020-07-23 22:45:45 -0300

    MongoDB

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const { Router } = require('express');
 
@@ -20,6 +23,13 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 export default routes;
