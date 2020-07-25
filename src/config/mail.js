84805bb8271ca4c92ad0e4277ef10dadbae51f46
Leonan Mattos <leocwb@MacBook-Air-de-Leonan.local>
2020-07-25 00:55:26 -0300

    Redis e e-mail configurados

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9646cdeedf027d',
+    pass: '7eee60f78882b5',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
